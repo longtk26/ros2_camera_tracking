7d472381598ff31ad9cf24a1ac880ce168672391
Pham Ngoc Long <ngoclong2663@gmail.com>
2025-03-13 14:13:04 +0700

    debug

--- opencv_tools/standley_node.py
+++ opencv_tools/standley_node.py
@@ -216,11 +216,11 @@ class StandleyNode(Node):
             e: end
         """
         try:
-            # self.get_logger().info(f"STM32 msg received in standley: {msg}")
             node_received = msg.split(":")[1]
             if node_received != "4":
                 return self.angle_imu, False
             
+            self.get_logger().info(f"STM32 msg received in standley: {msg}")
             data_received = msg.split(":")[2]
             return data_received, True
         except Exception as e:
