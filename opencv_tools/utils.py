cec00a4e33c5dc13cb46f322c906b4c61019edcc
longtk26 <longtk26@gmail.com>
2025-03-27 10:11:01 +0700

    feat: update distance calculate

--- opencv_tools/utils.py
+++ opencv_tools/utils.py
@@ -42,11 +42,13 @@ class Utils:
     
     def calculate_distance(self, position, current_frame, node_detect):
         x, y, w, h = position
-        real_high  = 1.63
-        measured_distance = 0.9 #meter 
+        real_high  = 0.5
+        measured_distance = 0.5 #meter 
         if node_detect.first_focal == 0:
             node_detect.focal_length = self.focal_length(high_in_image=h, real_high=real_high, measured_distance=measured_distance)
             node_detect.get_logger().info(f"FOCAL LENGTH FIRST................{node_detect.focal_length}")
+            with open("focal_length.txt", "w") as file:
+                file.write(str(node_detect.focal_length))
             node_detect.first_focal = 1
         
         distance = self.distance_finder(focal_length=node_detect.focal_length, real_high_person=real_high, high_person_in_frame=h)
