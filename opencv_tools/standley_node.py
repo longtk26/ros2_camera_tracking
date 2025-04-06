136c77c51f0d4f6a1359a7e35b83315f31d0e922
longtk26 <longtk26@gmail.com>
2025-04-06 08:44:46 +0200

    fix: update

--- opencv_tools/standley_node.py
+++ opencv_tools/standley_node.py
@@ -184,7 +184,7 @@ class StandleyNode(Node):
             
             
             # Step 5: Compute control angle delta
-            k = 0.4  # Gain parameter for crosstrack error
+            k = 0.3  # Gain parameter for crosstrack error
             ksoft = 0.8  # Small positive constant to avoid instability at low speed
             v = 0.3  # Assume velocity is 0.5 m/s (adjust if real velocity is available)
             theta_d = math.atan2(k * e_t, ksoft + v)
